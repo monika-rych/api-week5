d25d2e4725cc19ee050bf7e3bd529f737ef5ef7b
Monika <monika.k.rychlicka@gmail.com>
2020-06-16 00:58:51 +0100

    initial commit

---
+++ src/main/java/rych/monika/api/week5/ApiWeek5Application.java
@@ -0,0 +1,13 @@
+package rych.monika.api.week5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWeek5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiWeek5Application.class, args);
+    }
+
+}
---
+++ src/test/java/rych/monika/api/week5/ApiWeek5ApplicationTests.java
@@ -0,0 +1,13 @@
+package rych.monika.api.week5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiWeek5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
